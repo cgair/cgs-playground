09d1d5dc21699ef26c0d75d3b039fe4544685672
CGair23 <chengeaie23@sjtu.edu.cn>
2022-12-02 01:46:53 +0000

    Learning code about macro

--- Playground/compiler/hello.c
+++ Playground/compiler/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#define STR "Hello, world!\n"   // 变量式宏定义 (Object-like Macro)
+#define MAX ((a)>(b)?(a):(b))   // 函数式宏定义 (Function-like Macro)
+
 int main() {
-    printf("Hello, world!\n");
+    int k;
+    int a[] = {0, 1};
+
+    // k = MAX(a[0], a[1]);
+
+    printf(STR);
     return 0;
 }
